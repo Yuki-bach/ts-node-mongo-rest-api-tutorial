c293b7ac022c93dc3ef7897b63c99006a0d53e83
Yuki Ogawa <ogachan6464@gmail.com>
2024-03-05 10:24:00 -0700

    fix: typo

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,22 @@
-import * as express from 'express';
-import * as http from 'http';
-import * as bodyParser from 'body-parser';
-import * as cookieParser from 'cookie-parser';
-import * as compression from 'compression';
-import * as cors from 'cors';
+import express from "express";
+import http from "http";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import compression from "compression";
+import cors from "cors";
 
+// Setup Express Server
 const app = express();
-app.use(cors({
-  credentials: true,
-}));
-
+app.use(
+  cors({
+    credentials: true,
+  })
+);
 app.use(compression());
 app.use(cookieParser());
 app.use(bodyParser.json());
 
 const server = http.createServer(app);
-
 server.listen(8080, () => {
-  console.log("Server running on http://localhost:8080/")
+  console.log("Server running on http://localhost:8080/");
 });
